ca920cbb512feb567cac80c25b6c983d7e1e03bc
varun2216 <46109144+varun2216@users.noreply.github.com>
2023-10-10 16:00:30 +0530

    Update HelloWorld.java

--- src/main/java/gcp_cloud_function_poc/HelloWorld.java
+++ src/main/java/gcp_cloud_function_poc/HelloWorld.java
@@ -10,6 +10,7 @@ public class HelloWorld implements HttpFunction {
 
   @Override
   public void service(HttpRequest request, HttpResponse response) throws Exception {
+    
     BufferedWriter writer = response.getWriter();
     writer.write("Hello World!");
   }
