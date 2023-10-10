240f2d0bdd171b6d05a6064cbac5af5fd0a701d7
varun2216 <varun@digitaleast.mobi>
2023-10-10 15:41:58 +0530

    Initial commit

---
+++ src/main/java/gcp_cloud_function_poc/HelloWorld.java
@@ -0,0 +1,16 @@
+package gcp_cloud_function_poc;
+
+import java.io.BufferedWriter;
+
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+public class HelloWorld implements HttpFunction {
+
+  @Override
+  public void service(HttpRequest request, HttpResponse response) throws Exception {
+    BufferedWriter writer = response.getWriter();
+    writer.write("Hello World!");
+  }
+}
